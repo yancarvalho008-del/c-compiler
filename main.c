1a346bd300f92c913818a4305ae843e4c68f0fd6
yancarvalho008-del <yan.carvalho.008@gmail.com>
2026-09-17 08:48:39 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float tabela;
+int tabela [3][3];
+tabela[0][1];
+tabela[1][2];
+tabela[2][3];
+tabela[3][4];
+
+int main() {
+    for (contador = 0; contador < 7; contador++) {
+        printf("Digite os numeros da tabela %d: ", tabela + [3][3]);
+        scanf(%f - %f,tabela);
+    }
+    return 0;
+}
+
